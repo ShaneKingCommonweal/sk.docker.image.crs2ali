bbdb796d2387cef56ea14929d354fb7b7010f0d7
ShaneKing <github@azolla.org>
2019-08-04 02:14:17 +0800

    add use.sh

---
+++ use.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+pname="image.crs2ali.sh"
+user_registry="registry.cn-shanghai.aliyuncs.com/sk-sh"
+
+# L2
+for uname in `ls ./${pname}/cr2`
+do
+  for iname in `ls ./${pname}/cr2/${uname}`
+  do
+    for tname in `ls ./${pname}/cr2/${uname}/${iname}`
+    do
+      if [ -f ./${pname}/cr2/${uname}/${iname}/${tname}/checked.md ] ; then
+        docker pull ${user_registry}/${iname}:${tname}
+        docker tag ${user_registry}/${iname}:${tname} ${pname}/${uname}:${iname}
+      fi
+    done
+  done
+done
+
+# L3
+for cname in `ls ./${pname}/cr3`
+do
+  for uname in `ls ./${pname}/cr3/${cname}`
+  do
+    for iname in `ls ./${pname}/cr3/${cname}/${uname}`
+    do
+      for tname in `ls ./${pname}/cr3/${cname}/${uname}/${iname}`
+      do
+        if [ -f ./${pname}/cr3/${cname}/${uname}/${iname}/${tname}/checked.md ] ; then
+          docker pull ${user_registry}/${iname}:${tname}
+          docker tag ${user_registry}/${iname}:${tname} ${pname}/${cname}/${uname}:${iname}
+        fi
+      done
+    done
+  done
+done
